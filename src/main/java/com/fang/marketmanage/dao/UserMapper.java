70c0039d98c897cda5762b9cb1a5119dc10355c4
DarkFFang <45850696+DarkFFang@users.noreply.github.com>
2020-11-17 15:24:49 +0800

    修改Mapper方法

--- src/main/java/com/fang/marketmanage/dao/UserMapper.java
+++ src/main/java/com/fang/marketmanage/dao/UserMapper.java
@@ -9,17 +9,24 @@ import java.util.List;
 @Mapper
 public interface UserMapper {
     //公共方法
-    User login(String phone,String password);//登录
-    User update(String name,String phone,String password,String sex);//修改个人信息
+    User login(String phone, String password);//登录
+
+    User update(String name, String phone, String password, String sex);//修改个人信息
 
     //系统管理员
     int addNewUser(User user);//增加用户
+
     List<User> findUserList();//用户列表
+
     int deleteUserById(Integer id);//删除用户
+
     User updateUserById(Integer id);//修改用户
+
     List<User> findUserByUsername(String username);//通过用户名查询用户
-    List<User> findClerk();//搜索营业员列表
-    List<User> findKeeper();//搜索库管员列表
+
+    List<User> findClerkList();//搜索营业员列表
+
+    List<User> findKeeperList();//搜索库管员列表
 
 
 }
--- src/main/java/com/fang/marketmanage/dao/WorkTimeMapper.java
+++ src/main/java/com/fang/marketmanage/dao/WorkTimeMapper.java
@@ -1,13 +1,15 @@
 package com.fang.marketmanage.dao;
 
+import com.fang.marketmanage.entity.WorkTime;
+
 import java.util.List;
 
 public interface WorkTimeMapper {
-    int addWorkTime(WorkTimeMapper worktime);//增加时间表项
-    int daleteWorkTime(WorkTimeMapper worktime);//删除时间表项
-    WorkTimeMapper updateWorkTime();//修改时间表项
-    List<WorkTimeMapper> finWorkTimeList();//时间表列表
-    WorkTimeMapper searchWorkTimeByName(String username);//通过姓名查询时间表
-    List<WorkTimeMapper> searchClerkTime();//查询营业员时间表列表
-    List<WorkTimeMapper> searchKeeperTime();//查询库管员时间表列表
+    int addNewWorkTime(WorkTime worktime);//增加时间表项
+    int daleteWorkTimeById(Integer id);//删除时间表项
+    WorkTime updateWorkTimeById(Integer id);//修改时间表项
+    List<WorkTime> findWorkTimeList();//时间表列表
+    WorkTime searchWorkTimeByUsername(String username);//通过姓名查询时间表
+    List<WorkTime> findClerkTime();//查询营业员时间表列表
+    List<WorkTime> findKeeperTime();//查询库管员时间表列表
 }
