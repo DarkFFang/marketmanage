a7a3d34bc58e939cd7879e58e5ffdb9cae9d8ee7
DarkFFang <45850696+DarkFFang@users.noreply.github.com>
2020-11-17 13:47:30 +0800

    添加Good实体类成员属性

--- src/main/java/com/fang/marketmanage/entity/Good.java
+++ src/main/java/com/fang/marketmanage/entity/Good.java
@@ -1,5 +1,12 @@
 package com.fang.marketmanage.entity;
 
-public class Good {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
+public class Good {
+    private Integer id;
 }
