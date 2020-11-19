fa668572608563aa05f751fba22cc55dfc193e89
DarkFFang <45850696+DarkFFang@users.noreply.github.com>
2020-11-19 09:39:10 +0800

    添加swagger配置类

--- src/main/java/com/fang/marketmanage/controller/admin/UserManageController.java
+++ src/main/java/com/fang/marketmanage/controller/admin/UserManageController.java
@@ -2,26 +2,25 @@ package com.fang.marketmanage.controller.admin;
 
 import com.fang.marketmanage.entity.User;
 import com.fang.marketmanage.service.UserService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@Api("管理用户API接口")
+@RestController
 @RequestMapping("/admin/user")
 public class UserManageController {
     @Autowired
     UserService userService;
 
-    @RequestMapping("/toadd")
-    public String toadd(){
-        return "admin/add";
-    }
 
-    @RequestMapping("/add")
+    @PostMapping("/add")
     public String addNewUser(User user) {
         user.setPassword("111111");
         System.out.println(user.toString());
@@ -29,8 +28,7 @@ public class UserManageController {
         return "redirect:list";
     }
 
-    @RequestMapping("/list")
-    @ResponseBody
+    @GetMapping("/list")
     public List<User> findUserList(Model model) {
         List<User> userlist=userService.findUserList();
         model.addAttribute("list",userlist);
